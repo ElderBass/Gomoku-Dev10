9fdad64699b3357a6f40c9226e6b4046da150f23
Seth Zygarlicke <zygster11@gmail.com>
2021-05-28 10:53:53 -0500

    Initializing repo with given starter code for the assessment.

---
+++ src/main/java/learn/gomoku/App.java
@@ -0,0 +1,14 @@
+package learn.gomoku;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // prompt for human or random player for player 1
+        // if human, prompt for name
+        // pass the two players into Gomoku contstructor --> randomly selects who goes first
+        // print a randomizing message
+
+        // create a hashmap for handling  gameboard state?
+    }
+}
