7c368b0409d66611c22d242277891039845af39b
Seth Zygarlicke <zygster11@gmail.com>
2021-05-29 13:30:41 -0500

    Got my row/column grid printing the corresponding numbers for each row/col now!

--- src/main/java/learn/gomoku/UI/GameBoard.java
+++ src/main/java/learn/gomoku/UI/GameBoard.java
@@ -6,17 +6,16 @@ import java.util.List;
 
 public class GameBoard {
 
-    // capture stones that players successfully place
-    // pass these stones into the print board method
-    // might be in the form of the array of stones...?
-    // triple for-loop!
     private String blackSymbol = " O ";
     private String whiteSymbol = " X ";
     private String emptySymbol = " _ ";
-// TODO so this is printing an extra set of columns every time it renders but same number of rows...I have no idea...
+
     public void printGameBoard(List<Stone> stones) {
         System.out.println();
+        int count = 1;
+        System.out.println("  01 02 03 04 05 06 07 08 09 10 11 12 13 14 15");
         for (int row = 1; row < 16; row++) {
+            printRowNumber(count);
             for (int col = 1; col < 16; col++) {
                 if (getStoneAtPosition(stones, row, col) == null) {
                     System.out.print(emptySymbol);
@@ -26,11 +25,12 @@ public class GameBoard {
                     System.out.print(whiteSymbol);
                 }
             }
+            count++;
             System.out.println();
         }
         System.out.println();
     }
-    // maybe write another method that takes in a stone, a row, and a col, checks for a stone there, then returns the stone?
+
     public Stone getStoneAtPosition(List<Stone> stones, int row, int col) {
         for (Stone stone : stones) {
             if (stone.getRow() == row && stone.getColumn() == col) {
@@ -38,5 +38,12 @@ public class GameBoard {
             }
         }
         return null;
+    }
+    public void printRowNumber(int count) {
+        if (count >= 1 && count < 10) {
+            System.out.print("0" + count);
+        } else if (count >= 10) {
+            System.out.print(count);
+        }
     }
 }
