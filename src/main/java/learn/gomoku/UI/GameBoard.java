c0ed57aa157c62d4cd1c88d9cfbc016459f878b3
Seth Zygarlicke <zygster11@gmail.com>
2021-05-29 13:19:18 -0500

    Just debugged my print board method. It should be correctly printing the symbols according to players' moves. Now need to add a grid row around it...somehow...

--- src/main/java/learn/gomoku/UI/GameBoard.java
+++ src/main/java/learn/gomoku/UI/GameBoard.java
@@ -12,28 +12,31 @@ public class GameBoard {
     // triple for-loop!
     private String blackSymbol = " O ";
     private String whiteSymbol = " X ";
+    private String emptySymbol = " _ ";
 // TODO so this is printing an extra set of columns every time it renders but same number of rows...I have no idea...
     public void printGameBoard(List<Stone> stones) {
         System.out.println();
         for (int row = 1; row < 16; row++) {
             for (int col = 1; col < 16; col++) {
-
-                if (stones.isEmpty()) {
-                    System.out.print(" _ ");
-                } else {
-                    for (int index = 0; index < stones.size(); index++) {
-                        if (stones.get(index).isBlack() && stones.get(index).getRow() == row && stones.get(index).getColumn() == col) {
-                            System.out.print(blackSymbol);
-                        } else if (!stones.get(index).isBlack() && stones.get(index).getRow() == row && stones.get(index).getColumn() == col) {
-                            System.out.print(whiteSymbol);
-                        } else {
-                            System.out.print(" _ ");
-                        }
-                    }
+                if (getStoneAtPosition(stones, row, col) == null) {
+                    System.out.print(emptySymbol);
+                } else if (getStoneAtPosition(stones, row, col).isBlack()) {
+                    System.out.print(blackSymbol);
+                } else if (!getStoneAtPosition(stones, row, col).isBlack()) {
+                    System.out.print(whiteSymbol);
                 }
             }
             System.out.println();
         }
         System.out.println();
+    }
+    // maybe write another method that takes in a stone, a row, and a col, checks for a stone there, then returns the stone?
+    public Stone getStoneAtPosition(List<Stone> stones, int row, int col) {
+        for (Stone stone : stones) {
+            if (stone.getRow() == row && stone.getColumn() == col) {
+                return stone;
+            }
+        }
+        return null;
     }
 }
